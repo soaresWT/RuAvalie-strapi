b73101eb4d5f4f782a207f62da40559e1a06d624
soaresWT <victorsuares2014@gmail.com>
2023-07-01 09:53:29 -0300

    first commit

---
+++ src/api/votacao/controllers/votacao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * votacao controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::votacao.votacao');
---
+++ src/api/votacao/routes/votacao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * votacao router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::votacao.votacao');
